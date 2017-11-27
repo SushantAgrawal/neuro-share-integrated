703f9ef529f10a70ac6d5173f8cfc9e02f1d25a3
Anupam Bhattacharyya <abhattacharyya@netwovenindia.com>
2017-11-27 17:55:05 +0530

    temporary service method

--- src/app/neuro-graph/neuro-graph.service.ts
+++ src/app/neuro-graph/neuro-graph.service.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { URLSearchParams } from '@angular/http';
+import { URLSearchParams, RequestOptions, Headers } from '@angular/http';
 import * as moment from 'moment';
+import { SdkService, EndpointsService } from '@sutterhealth/data-services';
+import { SessionService } from '@sutterhealth/user-authentication';
 import { urlMaps, allMessages } from './neuro-graph.config';
 import { BrokerService } from './broker/broker.service';
+
 @Injectable()
 export class NeuroGraphService {
   global: any = {};
   moment: any;
-  constructor(private activatedRoute: ActivatedRoute, private brokerService: BrokerService) {
+  isIdle: boolean = true;
+  msUrlBase: string;
+  resourceMaps: {};
+  errorMessageId: string = 'data.service:error';
+  successMessageId: string = 'data.service:success';
+  warningMessageId: string = 'data.service:warning';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private sdk: SdkService,
+    private endpoints: EndpointsService,
+    private session: SessionService,
+    private brokerService: BrokerService
+  ) {
+
+    //Initialization
     this.set('urlMaps', urlMaps);
     this.brokerService.init(urlMaps);
-    this.moment = (moment as any).default
-      ? (moment as any).default
-      : moment;
+    this.moment = (moment as any).default ? (moment as any).default : moment;
     this.moment.locale('en');
 
     //url handling
@@ -29,8 +45,21 @@ export class NeuroGraphService {
     urlObject.encounter_status || (urlObject.encounter_status = 'Open');
     urlObject.csn || (urlObject.csn = "865482572");
     this.set('queryParams', urlObject);
+
+    //Environment Setup
+    this.sdk.getEnvironment().subscribe((env: string) => {
+      let url: string = '';
+      if (env === 'DEV') {
+        url = this.endpoints.getUrl('Dev');
+        url = `${this.endpoints.getUrl('Dev')}7ea2a62b33-neuroshareapis.apiary-mock.com`;
+      } else {
+        url = this.endpoints.getUrl('MaestroV2');
+      }
+      this.msUrlBase = `${url}/neuroshare/api/internal/ms`;
+    });
   }
 
+  // Getter Setter
   get(id) {
     return (this.global[id]);
   }
@@ -39,10 +68,12 @@ export class NeuroGraphService {
     this.global[id] = value;
   }
 
+  // Misc methods
   demographicBarPush(pushObject?: any) {
     this.brokerService.emit(allMessages.demographicEnableCheckBox, true);
   }
 
+  // Movable Popup 
   dragElement(elmnt) {
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     elmnt.onmousedown = dragMouseDown;
@@ -95,4 +126,52 @@ export class NeuroGraphService {
     }
     this.dragElement(divToMove);
   }
+
+  // Http methods
+  httpGet(
+    resourceId: string,
+    params?: { name: string, value: string }[],
+    headers?: { name: string, value: string }[],
+    carryBag?: any
+  ) {
+    try {
+      this.isIdle = false;
+      const resourcePath = this.resourceMaps[resourceId];
+      const url = `${this.msUrlBase}${resourcePath}`;
+      const options = new RequestOptions();
+      if (headers) {
+        const objHeaders = new Headers();
+        headers.map(x => objHeaders.append(x.name, x.value));
+        options.headers = objHeaders;
+      }
+      if (params) {
+        const objParams = new URLSearchParams();
+        params.map(x => objParams.append(x.name, x.value));
+        options.params = objParams;
+      }
+
+      // If SDK service needs the token to be passed
+      // this.session.getToken().subscribe(t => {
+      //   options.params.append('token', t);
+      //   this.sdk._requestGetWithToken(url, options).subscribe(d => {
+      //     //Send request
+      //   });
+      // });
+
+      //If SDK service sets the token
+      this.sdk._requestGet(url, options).subscribe(data => {
+        this.brokerService.emit(resourceId, data);
+        this.isIdle = true;
+      }, error => {
+        this.brokerService.emit(resourceId, { error: error });
+        this.brokerService.emit(this.errorMessageId, { error: error });
+        this.isIdle = true;
+      });
+
+    } catch (err) {
+      this.brokerService.emit(resourceId, { error: 'Unknown error encountered while making http get request.' });
+      this.brokerService.emit(this.errorMessageId, { error: 'Unknown error encountered while making http get request.' });
+      this.isIdle = true;
+    }
+  };
 }
