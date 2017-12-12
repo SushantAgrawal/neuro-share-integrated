e5098f705766e7bb1b1c772af0206b0bc89da224
Anupam Bhattacharyya <abhattacharyya@netwovenindia.com>
2017-12-12 12:22:11 +0530

    netwoven section enabled

--- src/assets/options.js
+++ src/assets/options.js
@@ -1,94 +1,94 @@
 var gUrlMaps = {
     //------------------------------------------ Sutter Health ----------------------------------------------------------------------------//
 
-    "http:get:walk25Feet": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/25walk/",
-    "http:post:walk25Feet": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/25walk",
-    "http:put:walk25Feet": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/25walk",
+    // "http:get:walk25Feet": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/25walk/",
+    // "http:post:walk25Feet": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/25walk",
+    // "http:put:walk25Feet": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/25walk",
 
-    "http:get:edss": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/edss-score/",
-    "http:post:edss": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/edss-score",
-    "http:put:edss": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/edss-score",
+    // "http:get:edss": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/edss-score/",
+    // "http:post:edss": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/edss-score",
+    // "http:put:edss": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/edss-score",
 
-    "http:get:relapse": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/relapses/",
-    "http:post:relapse": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/relapses",
-    "http:put:relapse": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/relapses",
+    // "http:get:relapse": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/relapses/",
+    // "http:post:relapse": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/relapses",
+    // "http:put:relapse": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/relapses",
 
-    "http:get:dmt": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/dmt/",
-    "http:post:dmt": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/dmt",
-    "http:put:dmt": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/dmt",
+    // "http:get:dmt": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/dmt/",
+    // "http:post:dmt": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/dmt",
+    // "http:put:dmt": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/dmt",
 
-    "http:get:otherMeds": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/other-meds/",
-    "http:post:otherMeds": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/other-meds",
-    "http:put:otherMeds": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/other-meds",
+    // "http:get:otherMeds": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/other-meds/",
+    // "http:post:otherMeds": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/other-meds",
+    // "http:put:otherMeds": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/other-meds",
 
-    "http:get:cds:user:data": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/cds/",
-    "http:post:cds:user:data": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/cds",
-    "http:put:cds:user:data": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/cds",
+    // "http:get:cds:user:data": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/cds/",
+    // "http:post:cds:user:data": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/cds",
+    // "http:put:cds:user:data": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/cds",
 
 
-    "http:get:all:questionnaire": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/all-questionnaire-responses/",
+    // "http:get:all:questionnaire": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/all-questionnaire-responses/",
 
-    "http:get:imaging": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/imaging-orders/",
+    // "http:get:imaging": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/imaging-orders/",
 
-    "http:get:labs": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/lab-orders/",
+    // "http:get:labs": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/lab-orders/",
 
-    "http:get:virtualCaseLoad": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/ms-population-data/",
+    // "http:get:virtualCaseLoad": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/ms-population-data/",
 
-    "http:get:walk25Feet:info": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/25walk-info/",
+    // "http:get:walk25Feet:info": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/25walk-info/",
 
-    "http:get:cds:info": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/cds-info/",
+    // "http:get:cds:info": "http://dcdlrhr901:8989/neuroshare/api/internal/ms/cds-info/",
 
-    "http:get:medications": "http://dcdlrhr901:8989/maestro/api/ehr/medications-orders/",
+    // "http:get:medications": "http://dcdlrhr901:8989/maestro/api/ehr/medications-orders/",
 
-    "http:get:encounters": "http://dcdlrhr901:8989/maestro/api/ehr/encounters/",
+    // "http:get:encounters": "http://dcdlrhr901:8989/maestro/api/ehr/encounters/",
 
-    "http:get:progressNote": "http://dcdlrhr901:8989/maestro/api/ehr/notes/",
+    // "http:get:progressNote": "http://dcdlrhr901:8989/maestro/api/ehr/notes/",
 
 
 
     //------------------------------------------ Netwoven ----------------------------------------------------------------------------//
 
-    // "http:get:walk25Feet": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/25walk/",
-    // "http:post:walk25Feet": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/25walk",
-    // "http:put:walk25Feet": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/25walk",
+    "http:get:walk25Feet": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/25walk/",
+    "http:post:walk25Feet": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/25walk",
+    "http:put:walk25Feet": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/25walk",
 
-    // "http:get:edss": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/edss-score/",
-    // "http:post:edss": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/edss-score",
-    // "http:put:edss": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/edss-score",
+    "http:get:edss": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/edss-score/",
+    "http:post:edss": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/edss-score",
+    "http:put:edss": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/edss-score",
 
-    // "http:get:relapse": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/relapses/",
-    // "http:post:relapse": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/relapses",
-    // "http:put:relapse": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/relapse",
+    "http:get:relapse": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/relapses/",
+    "http:post:relapse": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/relapses",
+    "http:put:relapse": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/relapse",
 
-    // "http:get:dmt": "http://61.16.131.166:3000/dmt/",
-    // "http:post:dmt": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/dmt",
-    // "http:put:dmt": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/dmt",
-
-
-    // "http:get:otherMeds": "http://61.16.131.166:3000/other-meds/",
-    // "http:post:otherMeds": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/other-meds",
-    // "http:put:otherMeds": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/other-meds",
-
-    // "http:get:cds:user:data": "http://61.16.131.166:3000/cds/",
-    // "http:post:cds:user:data": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/cds",
-    // "http:put:cds:user:data": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/cds",
+    "http:get:dmt": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/dmt/",
+    "http:post:dmt": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/dmt",
+    "http:put:dmt": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/dmt",
 
 
-    // "http:get:all:questionnaire": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/all-questionnaire-responses/",
+    "http:get:otherMeds": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/other-meds/",
+    "http:post:otherMeds": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/other-meds",
+    "http:put:otherMeds": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/other-meds",
 
-    // "http:get:imaging": "http://61.16.131.166:3000/imaging-orders/",
+    "http:get:cds:user:data": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/cds/",
+    "http:post:cds:user:data": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/cds",
+    "http:put:cds:user:data": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/cds",
 
-    // "http:get:labs": "http://61.16.131.166:3000/lab-orders/",
 
-    // "http:get:virtualCaseLoad": "http://61.16.131.166:3000/ms-population-data/",
+    "http:get:all:questionnaire": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/all-questionnaire-responses/",
 
-    // "http:get:walk25Feet:info": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/25walk-info/",
+    "http:get:imaging": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/imaging-orders/",
 
-    // "http:get:cds:info": "http://61.16.131.166:3000/cds-info/",
+    "http:get:labs": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/lab-orders/",
 
-    // "http:get:medications": "http://61.16.131.166:3000/medications-orders/",
+    "http:get:virtualCaseLoad": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/ms-population-data/",
 
-    // "http:get:encounters": "http://61.16.131.166:3000/encounters/",
+    "http:get:walk25Feet:info": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/25walk-info/",
 
-    // "http:get:progressNote": "http://61.16.131.166:3000/progress-note/",
+    "http:get:cds:info": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/cds-info/",
+
+    "http:get:medications": "http://61.16.131.166:3000/medications-orders/",
+
+    "http:get:encounters": "https://private-anon-72fed93c65-ehr2.apiary-mock.com/maestro/api/ehr/encounters/",
+
+    "http:get:progressNote": "https://private-anon-72fed93c65-ehr2.apiary-mock.com/maestro/api/ehr/notes/",
 };
