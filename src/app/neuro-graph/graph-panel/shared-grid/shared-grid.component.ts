e53f68d02fc82c180ceaa6c7d5b738b8a9539dc5
Debopoma Chaudhury <dchaudhury@netwoven.com>
2017-12-21 17:36:46 +0530

    D000126,Reference label adjustment

--- src/app/neuro-graph/graph-panel/shared-grid/shared-grid.component.ts
+++ src/app/neuro-graph/graph-panel/shared-grid/shared-grid.component.ts
@@ -281,7 +281,7 @@ export class SharedGridComponent implements OnInit, OnDestroy {
 
     if (today > currentDate) {
       let rectPrev = nodeSelection.append("rect")
-        .attr("x", xScale(previousDate) - 88)
+        .attr("x", xScale(previousDate) - 85)
         .attr("y", "20")
         .attr("width", lastOfficewidth)
         .attr("height", lastOfficeheight)
@@ -296,7 +296,7 @@ export class SharedGridComponent implements OnInit, OnDestroy {
         .style('font-size', '12px')
         .style('font-weight', 'bold')
       axisTextPrev.append('tspan')
-        .attr('x', xScale(previousDate) - 78)
+        .attr('x', xScale(previousDate) - 75)
         .attr('dy', 0)
         .text(this.lastOfficeDateLabel)
         .style('cursor', 'pointer')
@@ -306,7 +306,7 @@ export class SharedGridComponent implements OnInit, OnDestroy {
     }
     else {
       let rectPrev = nodeSelection.append("rect")
-        .attr("x", xScale(previousDate) - 88)
+        .attr("x", xScale(previousDate) - 85)
         .attr("y", "20")
         .attr("width", lastOfficewidth)
         .attr("height", lastOfficeheight)
@@ -322,11 +322,11 @@ export class SharedGridComponent implements OnInit, OnDestroy {
         .style('font-weight', 'bold')
         .style('cursor', 'pointer')
       axisTextPrev.append('tspan')
-        .attr('x', xScale(previousDate) - 63)
+        .attr('x', xScale(previousDate) - 60)
         .attr('dy', 0)
         .text(lastOfficeLabel1)
       axisTextPrev.append('tspan')
-        .attr('x', xScale(previousDate) - 78)
+        .attr('x', xScale(previousDate) - 75)
         .attr('dy', 15)
         .text(todayLastLabel)
         .on('click', d => {
@@ -346,7 +346,7 @@ export class SharedGridComponent implements OnInit, OnDestroy {
 
     if (today > currentDate) {
       let rect = nodeSelection.append("rect")
-        .attr("x", xScale(currentDate) + 2)
+        .attr("x", xScale(currentDate))
         .attr("y", "20")
         .attr("width", width)
         .attr("height", height)
@@ -357,13 +357,13 @@ export class SharedGridComponent implements OnInit, OnDestroy {
         .style('font-size', '12px')
         .style('font-weight', 'bold')
       axisText.append('tspan')
-        .attr('x', xScale(currentDate) + 8)
+        .attr('x', xScale(currentDate) + 6)
         .attr('dy', 0)
         .text(todayLabel)
     }
     else {
       let rect = nodeSelection.append("rect")
-        .attr("x", xScale(currentDate) + 2)
+        .attr("x", xScale(currentDate) )
         .attr("y", "20")
         .attr("width", width)
         .attr("height", height)
@@ -374,11 +374,11 @@ export class SharedGridComponent implements OnInit, OnDestroy {
         .style('font-size', '12px')
         .style('font-weight', 'bold')
       axisText.append('tspan')
-        .attr('x', xScale(currentDate) + 12)
+        .attr('x', xScale(currentDate) + 10)
         .attr('dy', 0)
         .text(todayLabel1)
       axisText.append('tspan')
-        .attr('x', xScale(currentDate) + 8)
+        .attr('x', xScale(currentDate) + 6)
         .attr('dy', 15)
         .text(todayLastLabel)
     }
