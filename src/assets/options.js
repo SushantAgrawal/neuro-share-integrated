0e0f4c4f670dc7bf6ada3f0d6b838dcdab420b41
Anupam Bhattacharyya <abhattacharyya@netwovenindia.com>
2017-12-28 15:40:03 +0530

    pointing to anupam's machine

--- src/assets/options.js
+++ src/assets/options.js
@@ -49,47 +49,47 @@ var gUrlMaps = {
     //------------------------------------------ Netwoven ----------------------------------------------------------------------------//
     
 
-    "http:get:walk25Feet": "http://61.16.131.166:3000/25walk/",
+    "http:get:walk25Feet": "http://10.10.1.33:3000/25walk/",
     "http:post:walk25Feet": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/25walk",
     "http:put:walk25Feet": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/25walk",
 
-    "http:get:edss": "http://61.16.131.166:3000/edss-score/",
+    "http:get:edss": "http://10.10.1.33:3000/edss-score/",
     "http:post:edss": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/edss-score",
     "http:put:edss": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/edss-score",
 
-    "http:get:relapse": "http://61.16.131.166:3000/relapses/",
+    "http:get:relapse": "http://10.10.1.33:3000/relapses/",
     "http:post:relapse": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/relapses",
     "http:put:relapse": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/relapse",
 
-    "http:get:dmt": "http://61.16.131.166:3000/dmt/",
+    "http:get:dmt": "http://10.10.1.33:3000/dmt/",
     "http:post:dmt": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/dmt",
     "http:put:dmt": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/dmt",
 
 
-    "http:get:otherMeds": "http://61.16.131.166:3000/other-meds/",
+    "http:get:otherMeds": "http://10.10.1.33:3000/other-meds/",
     "http:post:otherMeds": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/other-meds",
     "http:put:otherMeds": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/other-meds",
 
-    "http:get:cds:user:data": "http://61.16.131.166:3000/cds/",
+    "http:get:cds:user:data": "http://10.10.1.33:3000/cds/",
     "http:post:cds:user:data": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/cds",
     "http:put:cds:user:data": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/cds",
 
 
-    "http:get:all:questionnaire": "http://61.16.131.166:3000/all-questionnaire-responses/",
+    "http:get:all:questionnaire": "http://10.10.1.33:3000/all-questionnaire-responses/",
 
-    "http:get:imaging": "http://61.16.131.166:3000/imaging-orders/",
+    "http:get:imaging": "http://10.10.1.33:3000/imaging-orders/",
 
-    "http:get:labs": "http://61.16.131.166:3000/lab-orders/",
+    "http:get:labs": "http://10.10.1.33:3000/lab-orders/",
 
-    "http:get:virtualCaseLoad": "http://61.16.131.166:3000/ms-population-data/",
+    "http:get:virtualCaseLoad": "http://10.10.1.33:3000/ms-population-data/",
 
     "http:get:walk25Feet:info": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/ms/25walk-info/",
 
-    "http:get:cds:info": "http://61.16.131.166:3000/cds-info/",
+    "http:get:cds:info": "http://10.10.1.33:3000/cds-info/",
 
-    "http:get:medications": "http://61.16.131.166:3000/medications-orders/",
+    "http:get:medications": "http://10.10.1.33:3000/medications-orders/",
 
-    "http:get:encounters": "http://61.16.131.166:3000/encounters/",
+    "http:get:encounters": "http://10.10.1.33:3000/encounters/",
 
-    "http:get:progressNote": "http://61.16.131.166:3000/progress-note/",
+    "http:get:progressNote": "http://10.10.1.33:3000/progress-note/",
 };
